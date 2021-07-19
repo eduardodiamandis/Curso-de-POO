fbcd819be3a1b64b74614dd5f6f6deafcbbeb5a6
eduardodiamandis <72038107+eduardodiamandis@users.noreply.github.com>
2021-07-18 23:31:14 -0300

    Add files via upload
    Encapsulamento na pratica

---
+++ Aula6.java
@@ -0,0 +1,12 @@
+public class Aula6 {
+
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+      
+        c.maisVolume();
+        c.abrirMenu();
+       
+    }
+
+    
+}
